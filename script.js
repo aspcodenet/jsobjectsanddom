b190bec550a02e21a597ce1d200550954746cf7f
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-11-30 08:50:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Status???
+
+// 1 OBJECT ???
+// a) unnamed class
+// b) new function
+// c) class and constructor etc
+
+//some basic selectors, VISA Dagens övningar
+                    // GetElementById() ==================================================
+                    // GetElementByClassName() ===========================================
+                    // GetElementByTagName() =============================================
+                    // QuerySelector() - Väljer den FÖRSTA element som matchar  ==========
+
+// sätta lite styles etc!                    
+
+// array till table rows
+// på två olika sätt såklart
+
+// filter - när man skriver i Search så ska raderna automatiskt filtreras
+//    tjuvstart "events" - mer imorgon
+
+// (i morgon)
+// vid klick på en rad ska vi visa ett formulär (poppa upp en modal dialog) ( = edit )
+
+// om vi hinner idag?
+// skapa options -> select
+
+//egen tid = komma igång med övningarna
